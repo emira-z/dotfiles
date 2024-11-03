8c8ebe1cdc582b1658157a0095a5c444f96251ac
emily <emrami01@gmail.com>
2024-11-03 17:30:42 +0100

    [nvim] added buffer command

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.g.mapleader = "\\"
+
 require("catppuccin").setup({
     flavour = "macchiato",
 })
--- nvim/lua/plugins/bufremove.lua
+++ nvim/lua/plugins/bufremove.lua
@@ -2,4 +2,8 @@
 return {
     "echasnovski/mini.bufremove",
     version = false,
+    config = function()
+        -- Set up keybinding to delete buffer without closing Neovim
+        vim.api.nvim_set_keymap("n", "<leader>bd", "<cmd>lua MiniBufremove.delete(0, false)<CR>", { noremap = true })
+    end,
 }
